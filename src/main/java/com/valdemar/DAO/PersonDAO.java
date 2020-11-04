78808d5c0649bc29c2b5074a6287b49059501b6d
Valdemar <arnik.20100608@gmail.com>
2020-11-04 22:15:27 +0200

    Creat project(pattern DAO) GET and POST

---
+++ src/main/java/com/valdemar/DAO/PersonDAO.java
@@ -0,0 +1,33 @@
+package com.valdemar.DAO;
+
+import com.valdemar.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT,"Vova"));
+        people.add(new Person(++PEOPLE_COUNT,"Bob"));
+        people.add(new Person(++PEOPLE_COUNT,"Valdemar"));
+        people.add(new Person(++PEOPLE_COUNT,"Mike"));
+    }
+
+    public List<Person> index(){ return people; }
+
+    public Person show(int id){ return people.stream().filter(person-> person.getId()==id).findAny().orElse(null); }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+}
+
