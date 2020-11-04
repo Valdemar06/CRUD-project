fbb0a60126a51bdac63d714ed2c8ad69ec7e732a
Valdemar <arnik.20100608@gmail.com>
2020-11-04 18:59:56 +0200

    Add Request parameter and create calculator

--- src/main/java/com/valdemar/controllers/FirstController.java
+++ src/main/java/com/valdemar/controllers/FirstController.java
@@ -1,18 +1,51 @@
 package com.valdemar.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String sayHello(){return "first/hello";}
+    public String sayHello(@RequestParam(value = "name", required = false) String name,
+                           @RequestParam(value = "surname", required = false) String surname,
+                           Model model){
+       model.addAttribute("message", "Hello " + name + " " + surname);
+
+        return "first/hello";
+    }
 
     @GetMapping("/goodbye")
     public String goodbyePage(){
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("first_number") int firstNumber,
+                             @RequestParam("second_number") int secondNumber,
+                             @RequestParam("action") String action, Model model){
+        int result;
+        switch (action){
+            case "multiplication":
+                result = firstNumber * secondNumber;
+                break;
+            case "addition":
+                result = firstNumber + secondNumber;
+                break;
+            case "subtraction":
+                result = firstNumber - secondNumber;
+                break;
+            case "division":
+                result = firstNumber/secondNumber;
+                break;
+            default: result =0;
+        }
+        String endResult = "Action = "+ action + " Result = " + result;
+        model.addAttribute("result", endResult);
+        return "first/calculator";
     }
 }
