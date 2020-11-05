056751d79e27f3b9424834a75b1feebef2b8cee2
Valdemar <arnik.20100608@gmail.com>
2020-11-05 18:03:35 +0200

    ADD EDIT and DELETE

--- src/main/java/com/valdemar/config/MyConfigMVC.java
+++ src/main/java/com/valdemar/config/MyConfigMVC.java
@@ -1,6 +1,10 @@
 package com.valdemar.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class MyConfigMVC extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -14,7 +18,16 @@ public class MyConfigMVC extends AbstractAnnotationConfigDispatcherServletInitia
     }
 
     @Override
-    protected String[] getServletMappings() {
-        return new String[]{"/"};
+    protected String[] getServletMappings() { return new String[]{"/"}; }
+
+    @Override
+    public void onStartup(ServletContext aServletContext)throws ServletException{
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
     }
 }
